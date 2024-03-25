20327f06f07ef5be77be3a6ef2b243b75940094e
ASLobanov <arsentylobanov@yandex.ru>
2024-03-25 15:36:08 +0700

    swiper

--- src/js/price-repair.js
+++ src/js/price-repair.js
@@ -1,6 +1,5 @@
 import Swiper from 'swiper/swiper-bundle'
 import 'swiper/modules/pagination.scss'
-import 'swiper/modules/grid.scss'
 import 'swiper/swiper.scss'
 
 // Get width on browser window
@@ -8,22 +7,8 @@ const widthPriseRepair = window.innerWidth
 // Initialize Swiper
 if (widthPriseRepair < 767.9999) {
   var priseRepair = new Swiper('.price-repair__swiper', {
-    slidesPerView: 5,
-    //oneWayMovement: true,
-    //observer: true,
-    //observeParents: true,
-    //setWrapperSize: true,
-    // fill: 'column',
-    //fill: 'row',
-    // rows: 6,
-    //HTMLElement: 'price-repair__swiper-slide-one',
-
-    grid: {
-      fill: 'column',
-      rows: 5
-    },
     pagination: {
-      el: '.swiper-pagination,.price-repair__swiper__pagination'
+      el: '.swiper-pagination .price-repair__swiper__pagination'
     }
   })
 }
