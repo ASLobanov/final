125144ef213bc6f90fce12c4752e610029ffda1f
ASLobanov <arsentylobanov@yandex.ru>
2024-03-30 17:15:49 +0700

    fix menu

--- src/js/popap.js
+++ src/js/popap.js
@@ -4,22 +4,33 @@ const menuHeader = document.querySelector('.menu__header')
 const menuContact = document.querySelector('.menu__contact')
 const wrapperMain = document.querySelector('.wrapper-main')
 const footer = document.querySelector('.footer')
+const headerNav = document.querySelector('.header__nav')
+const divider = document.querySelector('.header__divider-two')
+const repairButton = document.querySelector('.button-repair')
 //console.log('buttonPopapMenu')
 
 buttonPopapMenu.addEventListener('click', () => {
   const headerMenu = buttonPopapMenu.classList.contains('open-menu')
 
   if (headerMenu) {
+    buttonPopapMenu.classList.remove('open-menu')
     asideMenu.classList.remove('open-menu')
     menuHeader.classList.remove('open-menu')
     menuContact.classList.remove('open-menu')
     wrapperMain.classList.remove('open-menu')
     footer.classList.remove('open-menu')
+    headerNav.classList.remove('open-menu')
+    divider.classList.remove('open-menu')
+    repairButton.classList.remove('open-menu')
   } else {
     asideMenu.classList.add('open-menu')
     menuHeader.classList.add('open-menu')
     menuContact.classList.add('open-menu')
     wrapperMain.classList.add('open-menu')
     footer.classList.add('open-menu')
+    headerNav.classList.add('open-menu')
+    divider.classList.add('open-menu')
+    repairButton.classList.add('open-menu')
+    buttonPopapMenu.classList.add('open-menu')
   }
 })
