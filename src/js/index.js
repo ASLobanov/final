af93e22acb0fc9cf9120cd2f6cdd36eb42c4d7f3
ASLobanov <arsentylobanov@yandex.ru>
2024-04-02 17:22:06 +0700

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,8 @@ import '../scss/style.scss'
 import '../js/types.js'
 import '../js/technology.js'
 import '../js/price-repair.js'
-import '../js/popap.js'
+import '../js/menuPopap.js'
 import '../js/brandButton.js'
+import '../js/popap.js'
 
 console.log('Works!')
