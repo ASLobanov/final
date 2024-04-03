9542127037f5b4cce9c9583c7fa8f53a3c5ff39a
ASLobanov <arsentylobanov@yandex.ru>
2024-04-03 15:05:12 +0700

    fix

--- src/js/popap.js
+++ src/js/popap.js
@@ -3,10 +3,20 @@ const openApplicationPopap = document.querySelector('.feedback')
 const closeButtonPopup = document.querySelector(
   '.feedback__button-close-active'
 )
+const opasityMain = document.querySelector('.main')
+const opasityFooter = document.querySelector('.footer')
+const wrapperBody = document.querySelector('.wrapper')
 
 applicationPopap.addEventListener('click', () => {
+  opasityMain.classList.add('feedback-open')
   openApplicationPopap.classList.add('feedback-open')
+  opasityFooter.classList.add('feedback-open')
+  wrapperBody.classList.add('feedback-open')
 })
+
 closeButtonPopup.addEventListener('click', () => {
   openApplicationPopap.classList.remove('feedback-open')
+  opasityMain.classList.remove('feedback-open')
+  opasityFooter.classList.remove('feedback-open')
+  wrapperBody.classList.remove('feedback-open')
 })
